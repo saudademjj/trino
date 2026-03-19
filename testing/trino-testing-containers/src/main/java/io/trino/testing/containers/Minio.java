65d16613a9dfa99205954d9f017b45351e35d372
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-03-19 10:25:08 +0100

    Fix flaky Iceberg testBloomFilterColumnWithDictionaryPage
    By making sure that test tables are independent from each other. This requires a copied manifest to be patched for
    unique table location.

--- testing/trino-testing-containers/src/main/java/io/trino/testing/containers/Minio.java
+++ testing/trino-testing-containers/src/main/java/io/trino/testing/containers/Minio.java
@@ -148,6 +148,22 @@ public class Minio
         }
     }
 
+    public void processAndCopyResources(String resourcePath, ResourcePreProcessor processor, String bucketName, String target)
+    {
+        try (MinioClient minioClient = createMinioClient()) {
+            for (ClassPath.ResourceInfo resourceInfo : ClassPath.from(getClass().getClassLoader())
+                    .getResources()) {
+                if (resourceInfo.getResourceName().startsWith(resourcePath)) {
+                    String fileName = resourceInfo.getResourceName().replaceFirst("^" + Pattern.quote(resourcePath), quoteReplacement(target));
+                    minioClient.putObject(bucketName, processor.process(fileName, resourceInfo.asByteSource().read()), fileName);
+                }
+            }
+        }
+        catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
     public void writeFile(byte[] contents, String bucketName, String path)
     {
         try (MinioClient minioClient = createMinioClient()) {
@@ -182,5 +198,10 @@ public class Minio
         {
             return new Minio(image, hostName, exposePorts, filesToMount, envVars, network, startupRetryLimit);
         }
+    }
+
+    public interface ResourcePreProcessor
+    {
+        byte[] process(String name, byte[] data);
     }
 }
